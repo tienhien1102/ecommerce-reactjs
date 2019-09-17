b33e00d63f48f88f4616f253cdee47ccc2e01976
tienhien1102 <tienhien110293@gmail.com>
2019-09-17 16:16:41 +0700

    initial data

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+//app 
+const app = express();
+
+//db 
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => console.log('data connected'))
+
+app.get('/', (req, res) => {
+    res.send("hello from node update");
+});
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
